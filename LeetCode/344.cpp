bdee0421bb7a57a893b467d37d05f08a78c0852c
Zhangsongchao <614984177@qq.com>
2019-05-14 14:05:02 +0800

    单词反转

--- LeetCode/344.cpp
+++ LeetCode/344.cpp
@@ -4,29 +4,29 @@ const int MAXN = 1e5 + 5;
 class Solution 
 {
 public:
-    void reverseString(vector<char>& s) 
+    string reverseWords(string s) 
     {
-        for (int i = 0; i < s.size() / 2; i++)
-            swap(s[i], s[s.size() - i - 1]);
+        string ans;
+        stringstream ss(s);
+        while (ss >> s) 
+        {
+            reverse(s.begin(), s.end());
+            if (ans != "") ans += " ";
+            ans += s;
+        }
+        return ans;
     }
 };
 int main()
 {
     Solution AC;
-    int n;
-    while (cin >> n)
+    string s;
+    while (cin >> s)
     {
-        vector<char> a(n, '\0');
-        for (int i = 0; i < n; i++) cin >> a[i];
-        AC.reverseString(a);
-        for (auto i : a) cout << i;
-        cout << endl;
+        cout << AC.reverseWords(s) << endl;
     }
     return 0;
 }
 /*
-5
-hello
-6
-Hannah
+Let's take LeetCode contest
 */
