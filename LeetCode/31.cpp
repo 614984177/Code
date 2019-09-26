c6306f7c76a438f67933b6921c28194a1bcd6c4f
Zhangsongchao <614984177@qq.com>
2019-09-26 14:14:52 +0800

    字典序下一个排列

---
+++ LeetCode/31.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAXN = 1e5 + 5;
+class Solution 
+{
+public:
+    void nextPermutation(vector<int>& nums) 
+    {
+        if (nums.size() <= 1) return ;
+        int i = nums.size() - 1;
+        while (i - 1 >= 0 && nums[i - 1] >= nums[i]) i--;
+        reverse(nums.begin() + i, nums.end());
+        for (auto i : nums) cout << i << " ";
+        cout << endl;
+        if (i-- != 0)
+        {
+            auto j = upper_bound(nums.begin() + i, nums.end(), nums[i]);
+            swap(nums[i], *j);
+        }
+    }
+};
+
+int main()
+{
+    Solution AC;
+    int n; cin >> n;
+    vector<int> a(n, -1);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    AC.nextPermutation(a);
+    for (auto i : a) cout << i << " ";
+    cout << endl;
+    return 0;
+}
+/*
+3
+1 2 3
+*/
