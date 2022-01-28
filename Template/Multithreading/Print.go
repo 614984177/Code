04d339bde3c94465546389c0167886614b0827cd
614984177 <614984177@qq.com>
2022-01-28 16:51:14 +0800

    go多协程打印相关

--- Template/Multithreading/Print.go
+++ Template/Multithreading/Print.go
@@ -5,26 +5,35 @@ import (
 	"sync"
 )
 
+// PrintOddEven, 启动两个协程分别打印奇偶数
 func PrintOddEven() {
 	num := 10
 	var wg sync.WaitGroup
 	ch := make(chan struct{})
 	wg.Add(2)
 	go func() {
-		defer wg.Done()
+		fmt.Println("PrintEven Goroutine start...")
+		defer func() {
+			wg.Done()
+			fmt.Println("PrintEven Goroutine end!")
+		}()
 		for i := 0; i < num; i++ {
 			ch <- struct{}{}
 			if i%2 == 0 {
-				fmt.Print(i, " ")
+				fmt.Println("PrintEven Goroutine: ", i)
 			}
 		}
 	}()
 	go func() {
-		defer wg.Done()
+		fmt.Println("PrintOdd  Goroutine start...")
+		defer func() {
+			wg.Done()
+			fmt.Println("PrintOdd  Goroutine end!")
+		}()
 		for i := 0; i < num; i++ {
 			<-ch
 			if i%2 == 1 {
-				fmt.Print(i, " ")
+				fmt.Println("PrintOdd  Goroutine: ", i)
 			}
 		}
 	}()
@@ -32,33 +41,46 @@ func PrintOddEven() {
 	fmt.Println()
 }
 
+// PrintOddEven, 启动三个协程分别打印ABC
 func PrintABC() {
-	num := 3
 	var wg sync.WaitGroup
 	a, b, c := make(chan struct{}), make(chan struct{}), make(chan struct{})
-	wg.Add(num)
+	wg.Add(3)
+	num := 3
 	go func() {
-		defer wg.Done()
+		fmt.Println("PrintA Goroutine start...")
+		defer func() {
+			wg.Done()
+			fmt.Println("PrintA Goroutine end!")
+		}()
 		for i := 0; i < num; i++ {
 			<-a
-			fmt.Print("A")
+			fmt.Println("PrintA Goroutine: A")
 			b <- struct{}{}
 		}
 		// <-a
 	}()
 	go func() {
-		defer wg.Done()
+		fmt.Println("PrintB Goroutine start...")
+		defer func() {
+			wg.Done()
+			fmt.Println("PrintB Goroutine end!")
+		}()
 		for i := 0; i < num; i++ {
 			<-b
-			fmt.Print("B")
+			fmt.Println("PrintB Goroutine: B")
 			c <- struct{}{}
 		}
 	}()
 	go func() {
-		defer wg.Done()
+		fmt.Println("PrintC Goroutine start...")
+		defer func() {
+			wg.Done()
+			fmt.Println("PrintC Goroutine end!")
+		}()
 		for i := 0; i < num; i++ {
 			<-c
-			fmt.Print("C")
+			fmt.Println("PrintC Goroutine: C")
 			if i != 2 {
 				a <- struct{}{}
 			}
@@ -67,8 +89,10 @@ func PrintABC() {
 	a <- struct{}{}
 	wg.Wait()
 }
+
+// PrintOddEven, 启动N个协程按顺序打印数字
 func PrintNum() {
-	num := 100
+	num := 102
 	exitChan := make(chan struct{})
 	chanNum := 10
 	chanQueue := make([]chan struct{}, chanNum)
@@ -76,23 +100,31 @@ func PrintNum() {
 	for i := 0; i < chanNum; i++ {
 		chanQueue[i] = make(chan struct{})
 	}
-	go func() {
-		chanQueue[0] <- struct{}{}
-	}()
+	lastPos := (num - 1 + chanNum) % chanNum
+	fmt.Println("End Pos is: ", lastPos)
 	for i := 0; i < chanNum; i++ {
-		curChan := chanQueue[i]
-		nextChan := chanQueue[(i+1)%chanNum]
-		go func(i int, cur, next chan struct{}) {
-			for j := 0; j < num/chanNum; j++ {
+		go func(i int) {
+			cur := chanQueue[i]
+			next := chanQueue[(i+1)%chanNum]
+			name := fmt.Sprintf("%d Goroutine", i)
+			fmt.Printf("%s start...\n", name)
+			defer fmt.Printf("%s end!\n", name)
+			len := num / chanNum
+			if i <= lastPos && lastPos != chanNum-1 {
+				len += 1
+			}
+			for j := 0; j < len; j++ {
 				<-cur
-				fmt.Print(i*num/chanNum+j, " ")
+				fmt.Printf("%s: %d\n", name, j*(num/chanNum)+i)
+				if i == lastPos && j == len-1 {
+					exitChan <- struct{}{}
+					continue
+				}
 				next <- struct{}{}
 			}
-			if i == chanNum-1 {
-				exitChan <- struct{}{}
-			}
-		}(i, curChan, nextChan)
+		}(i)
 	}
+	chanQueue[0] <- struct{}{}
 	<-exitChan
 }
 func main() {
