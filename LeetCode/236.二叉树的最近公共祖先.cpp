1d22be71730a38672c6a05a9070be37bdc5cc5b3
zhangsongchao <614984177@qq.com>
2022-02-15 23:29:02 +0800

    LCA

---
+++ LeetCode/236.二叉树的最近公共祖先.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode.cn id=236 lang=cpp
+ *
+ * [236] 二叉树的最近公共祖先
+ */
+
+// @lc code=start
+
+// Definition for a binary tree node.
+// struct TreeNode {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+// };
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root) return nullptr;
+        if (root == p || root == q) return root;
+        TreeNode* l = lowestCommonAncestor(root->left, p, q);
+        TreeNode* r = lowestCommonAncestor(root->right, p, q);
+        if (l != nullptr && r != nullptr) return root;
+        else if (l != nullptr) return l;
+        else if (r != nullptr) return r;
+        return nullptr;
+    }
+};
+// @lc code=end
+
